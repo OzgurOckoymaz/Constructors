5b0128c6630ca0b4b0ffcdcdbe1f3248dac58873
Özgür <ozgur.engineering@gmail.com>
2021-01-17 11:28:25 +0200

    Add project files.

---
+++ Constructors/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            Triangle t = new Triangle(10, 20);
+            t.H = Convert.ToDecimal(textBox1.Text);
+            t.Bottom = Convert.ToDecimal(textBox2.Text);
+
+            MessageBox.Show($" The area of the triangle is {t.area_triangle} ");
+
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void btnPerimeter_Click(object sender, EventArgs e)
+        {
+            Triangle t = new Triangle(1, 2);
+            t.Left = Convert.ToDecimal(textBox3.Text);
+            t.Right = Convert.ToDecimal(textBox4.Text);
+            t.Bottom = Convert.ToDecimal(textBox5.Text);
+            MessageBox.Show($"the perimeter of the triangle is {t.perimeter_triangle(t.Left, t.Right, t.Bottom)}");
+
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+        }
+    }
+}
---
+++ Constructors/Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Triangle
+    {
+        public Triangle(decimal height, decimal bottom)
+        {
+            H = height;
+            Bottom = bottom;
+        }
+
+        private decimal _h;
+        private decimal _bottom;
+        private decimal _left;
+        private decimal _right;
+
+        public decimal H
+        {
+            get { return _h; }
+            set { _h = value; }
+        }
+
+        public decimal Bottom
+        {
+            get { return _bottom; }
+            set { _bottom = value; }
+        }
+
+        public decimal Right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+
+        public decimal Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+
+        public decimal area_triangle
+        {
+            get { return (_bottom * _h) / 2; }
+        }
+
+        public decimal perimeter_triangle(decimal a, decimal b, decimal c)
+        {
+            decimal per = a + b + c;
+            return per;
+        }
+
+    }
+}
